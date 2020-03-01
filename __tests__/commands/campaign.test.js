1e6cc2d2735eb4a4420a081e032c9acc89254906
shine <aungshine@gmail.com>
2020-03-01 13:47:54 -0800

    add default case to campaign sub command + test

--- __tests__/commands/campaign.test.js
+++ __tests__/commands/campaign.test.js
@@ -44,7 +44,7 @@ describe('campaign : status command Unit Test', () => {
     msg.channel.send.mockClear()
   })
 
-  test('display the correct status message based on current campaign ', async () => {
+  test('display the correct status message based on current campaign in Nomination Phase ', async () => {
     getCurrentCampaign.mockReturnValueOnce(testCampaign)
 
     const expectedEmbed = {
@@ -52,7 +52,7 @@ describe('campaign : status command Unit Test', () => {
         name: 'Election Bot'
       },
       color: 39423,
-      description: `Currently in : ${currentPhase} \n This Phase will end ${currentPhase === 'Nomination Phase' ? moment(testCampaign.nominationPeriod).fromNow() : moment(testCampaign.votingPeriod).fromNow()}`
+      description: `Currently in : ${currentPhase} \n This Phase will end ${moment(testCampaign.nominationPeriod).fromNow()}`
     }
     const expectedFields = {
       value: `Role to confer : ${testCampaign.targetRole}
@@ -72,6 +72,39 @@ describe('campaign : status command Unit Test', () => {
       expect.objectContaining(expectedFields)
     )
   })
+
+  test('display the correct status message based on current campaign in Voting Phase ', async () => {
+    getCurrentCampaign.mockReturnValueOnce(testCampaign)
+
+    const currentPhase = 'Voting Phase'
+    testCampaign.currentPhase = currentPhase
+
+    const expectedEmbed = {
+      author: {
+        name: 'Election Bot'
+      },
+      color: 39423,
+      description: `Currently in : ${currentPhase} \n This Phase will end ${moment(testCampaign.votingPeriod).fromNow()}`
+    }
+    const expectedFields = {
+      value: `Role to confer : ${testCampaign.targetRole}
+        Number of open role: ${testCampaign.openRoleCount}
+        Number of nominee slot: ${testCampaign.nominationSlot}
+        Nomination Period will end at : ${moment(testCampaign.nominationPeriod)}
+        Voting Period will end at : ${moment(testCampaign.votingPeriod)}
+        Campaign Created by ${testCampaign.creator}`
+    }
+
+    await campaign.execute(msg, args)
+    expect(msg.channel.send.mock.calls.length).toBe(2)
+    expect(msg.channel.send.mock.calls[1][0].embed).toEqual(
+      expect.objectContaining(expectedEmbed)
+    )
+    expect(msg.channel.send.mock.calls[1][0].embed.fields[0]).toEqual(
+      expect.objectContaining(expectedFields)
+    )
+  })
+
   test('error is returned if no current campaign exists', async () => {
     getCurrentCampaign.mockReturnValueOnce()
 
@@ -238,5 +271,24 @@ describe('campaign : cancel command Unit Test', () => {
     expect(msg.channel.send.mock.calls.length).toBe(2)
     expect(msg.channel.send.mock.calls[1][0]).toEqual(expectedError)
     expect(cancelCurrentCampaign.mock.calls.length).toBe(0)
+  })
+})
+
+describe('campaign : unknown sub-command Unit Test', () => {
+  const msg = Helper.mockMessage({})
+  const args = ['unknown']
+
+  const expectedError = 'Unknown Sub Command provided, please use "!election help" to see usage guide'
+
+  beforeEach(() => {
+    msg.reply.mockClear()
+  })
+
+  test('proper error is reply to the sender', async () => {
+    getCurrentCampaign.mockReturnValueOnce(testCampaign)
+
+    await campaign.execute(msg, args)
+    expect(msg.reply.mock.calls.length).toBe(1)
+    expect(msg.reply.mock.calls[0][0]).toEqual(expectedError)
   })
 })
